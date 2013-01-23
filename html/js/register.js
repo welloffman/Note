823a34dbf854727652597a93e8f36af333c6d787
welloffman <welloffman@yandex.ru>
2013-01-23 22:51:25 +0100

    fix

--- html/js/register.js
+++ html/js/register.js
@@ -3,8 +3,8 @@ $("document").ready(function() {
 	// Нажатие на кнопку "отправить" в форме авторизации
 	$(".login-button").bind('click', function() {
 		var data = {
-			"login": $("form.log_form input[name='login']").val(),
-			"passwd": $("form.log_form input[name='passwd']").val()
+			"login": $("form.login input[name='login']").val(),
+			"passwd": $("form.login input[name='passwd']").val()
 		};
 		
 		$.post("/Register/reg", data, function(result) {
